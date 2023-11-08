ae3033c8187f79bdf40e79094657f55405764fec
muhdastinfauzi <muhammaddastinfauzi@gmail.com>
2023-11-08 08:15:50 +0700

    feat: create table-pesanan

---
+++ laundryku/components/table-pesanan.tsx
@@ -0,0 +1,94 @@
+export default function TablePesanan() {
+  return (
+    <div className="relative overflow-x-auto mt-8">
+      <table className="w-full text-center">
+        <thead className="text-lg uppercase bg-[#7689E7]">
+          <tr>
+            <th scope="col" className="px-6 py-3 font-medium">
+              ID
+            </th>
+            <th scope="col" className="px-6 py-3 font-medium">
+              ID User
+            </th>
+            <th scope="col" className="px-12 py-3 font-medium">
+              Items
+            </th>
+            <th scope="col" className="px-12 py-3 font-medium">
+              Waktu Pemesanan
+            </th>
+            <th scope="col" className="px-6 py-3 font-medium">
+              Nominal Pesanan
+            </th>
+            <th scope="col" className="px-6 py-3 font-medium">
+              Status Pesanan
+            </th>
+            <th scope="col" className="px-6 py-3 font-medium">
+              Status Pembayaran
+            </th>
+            <th scope="col" className="px-6 py-3 font-medium">
+              Bukti Pembayaran
+            </th>
+            <th scope="col" className="px-6 py-3 font-medium"></th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr className="bg-white">
+            <td className="px-3 py-4">1</td>
+            <td className="px-3 py-4">1</td>
+            <td className="px-3 py-4">2 x Kemeja, 1 x Ransel</td>
+            <td className="px-3 py-4">2023-10-26 23:26:00</td>
+            <td className="px-3 py-4">Rp10.000</td>
+            <td className="px-3 py-4">Dalam Proses</td>
+            <td className="px-3 py-4">Sudah Dibayar</td>
+            <td className="px-3 py-4">Lihat</td>
+            <td className="px-6 py-4">Edit</td>
+          </tr>
+          <tr className="bg-white">
+            <td className="px-3 py-4">1</td>
+            <td className="px-3 py-4">1</td>
+            <td className="px-3 py-4">2 x Kemeja, 1 x Ransel</td>
+            <td className="px-3 py-4">2023-10-26 23:26:00</td>
+            <td className="px-3 py-4">Rp10.000</td>
+            <td className="px-3 py-4">Dalam Proses</td>
+            <td className="px-3 py-4">Sudah Dibayar</td>
+            <td className="px-3 py-4">Lihat</td>
+            <td className="px-6 py-4">Edit</td>
+          </tr>
+          <tr className="bg-white">
+            <td className="px-3 py-4">1</td>
+            <td className="px-3 py-4">1</td>
+            <td className="px-3 py-4">2 x Kemeja, 1 x Ransel</td>
+            <td className="px-3 py-4">2023-10-26 23:26:00</td>
+            <td className="px-3 py-4">Rp10.000</td>
+            <td className="px-3 py-4">Dalam Proses</td>
+            <td className="px-3 py-4">Sudah Dibayar</td>
+            <td className="px-3 py-4">Lihat</td>
+            <td className="px-6 py-4">Edit</td>
+          </tr>
+          <tr className="bg-white">
+            <td className="px-3 py-4">1</td>
+            <td className="px-3 py-4">1</td>
+            <td className="px-3 py-4">2 x Kemeja, 1 x Ransel</td>
+            <td className="px-3 py-4">2023-10-26 23:26:00</td>
+            <td className="px-3 py-4">Rp10.000</td>
+            <td className="px-3 py-4">Dalam Proses</td>
+            <td className="px-3 py-4">Sudah Dibayar</td>
+            <td className="px-3 py-4">Lihat</td>
+            <td className="px-6 py-4">Edit</td>
+          </tr>
+          <tr className="bg-white">
+            <td className="px-3 py-4">1</td>
+            <td className="px-3 py-4">1</td>
+            <td className="px-3 py-4">2 x Kemeja, 1 x Ransel</td>
+            <td className="px-3 py-4">2023-10-26 23:26:00</td>
+            <td className="px-3 py-4">Rp10.000</td>
+            <td className="px-3 py-4">Dalam Proses</td>
+            <td className="px-3 py-4">Sudah Dibayar</td>
+            <td className="px-3 py-4">Lihat</td>
+            <td className="px-6 py-4">Edit</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+}
