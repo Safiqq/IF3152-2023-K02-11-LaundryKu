4ae371f64f3882f852f3ddee2c1f1792f1a25ae5
muhdastinfauzi <muhammaddastinfauzi@gmail.com>
2023-11-17 10:06:02 +0700

    feat: update table-pesanan

--- laundryku/components/table-pesanan.tsx
+++ laundryku/components/table-pesanan.tsx
@@ -1,92 +1,113 @@
+import button from "./button"
+
 export default function TablePesanan() {
+  const btnStatPemesanan = () => {
+    return (
+      button('Dalam Pesanan', '#BCBBB1', 'black')
+    )
+  }
+
+  const btnStatPembayaran = () => {
+    return (
+      button('Sudah Dibayar', '#227B3D', 'white')
+    )
+  }
+
+  const data = [
+    {
+      "ID": 1,
+      "ID USER": 1,
+      "ITEMS": '2 x Kemeja, 1 x Ransel',
+      "WAKTU PEMESANAN": '2023-10-26 23:26:00',
+      "NOMINAL PESANAN": 'Rp10.000',
+      "STATUS PESANAN": btnStatPemesanan(),
+      "STATUS PEMBAYARAN": btnStatPembayaran(),
+      "BUKTI PEMBAYARAN": 'Lihat',
+      "": 'Edit'
+    },
+    {
+      "ID": 2,
+      "ID USER": 2,
+      "ITEMS": '2 x Kemeja, 1 x Ransel',
+      "WAKTU PEMESANAN": '2023-10-26 23:26:00',
+      "NOMINAL PESANAN": 'Rp10.000',
+      "STATUS PESANAN": btnStatPemesanan(),
+      "STATUS PEMBAYARAN": btnStatPembayaran(),
+      "BUKTI PEMBAYARAN": 'Lihat',
+      "": 'Edit'
+    },
+    {
+      "ID": 3,
+      "ID USER": 3,
+      "ITEMS": '2 x Kemeja, 1 x Ransel',
+      "WAKTU PEMESANAN": '2023-10-26 23:26:00',
+      "NOMINAL PESANAN": 'Rp10.000',
+      "STATUS PESANAN": btnStatPemesanan(),
+      "STATUS PEMBAYARAN": btnStatPembayaran(),
+      "BUKTI PEMBAYARAN": 'Lihat',
+      "": 'Edit'
+    },
+    {
+      "ID": 4,
+      "ID USER": 4,
+      "ITEMS": '2 x Kemeja, 1 x Ransel',
+      "WAKTU PEMESANAN": '2023-10-26 23:26:00',
+      "NOMINAL PESANAN": 'Rp10.000',
+      "STATUS PESANAN": btnStatPemesanan(),
+      "STATUS PEMBAYARAN": btnStatPembayaran(),
+      "BUKTI PEMBAYARAN": 'Lihat',
+      "": 'Edit'
+    },
+    {
+      "ID": 5,
+      "ID USER": 5,
+      "ITEMS": '2 x Kemeja, 1 x Ransel',
+      "WAKTU PEMESANAN": '2023-10-26 23:26:00',
+      "NOMINAL PESANAN": 'Rp10.000',
+      "STATUS PESANAN": btnStatPemesanan(),
+      "STATUS PEMBAYARAN": btnStatPembayaran(),
+      "BUKTI PEMBAYARAN": 'Lihat',
+      "": 'Edit'
+    },
+    {
+      "ID": 6,
+      "ID USER": 6,
+      "ITEMS": '2 x Kemeja, 1 x Ransel',
+      "WAKTU PEMESANAN": '2023-10-26 23:26:00',
+      "NOMINAL PESANAN": 'Rp10.000',
+      "STATUS PESANAN": btnStatPemesanan(),
+      "STATUS PEMBAYARAN": btnStatPembayaran(),
+      "BUKTI PEMBAYARAN": 'Lihat',
+      "": 'Edit'
+    },
+  ]
+
+  const titles = Object.keys(data[0])
+
   return (
     <div className="relative overflow-x-auto mt-8">
       <table className="w-full text-center">
         <thead className="text-lg uppercase bg-[#7689E7]">
           <tr>
-            <th scope="col" className="px-6 py-3 font-medium">
-              ID
-            </th>
-            <th scope="col" className="px-6 py-3 font-medium">
-              ID User
-            </th>
-            <th scope="col" className="px-12 py-3 font-medium">
-              Items
-            </th>
-            <th scope="col" className="px-12 py-3 font-medium">
-              Waktu Pemesanan
-            </th>
-            <th scope="col" className="px-6 py-3 font-medium">
-              Nominal Pesanan
-            </th>
-            <th scope="col" className="px-6 py-3 font-medium">
-              Status Pesanan
-            </th>
-            <th scope="col" className="px-6 py-3 font-medium">
-              Status Pembayaran
-            </th>
-            <th scope="col" className="px-6 py-3 font-medium">
-              Bukti Pembayaran
-            </th>
-            <th scope="col" className="px-6 py-3 font-medium"></th>
+            {titles.map((item) => (
+              <th scope="col" className="px-6 py-3 font-medium">{item}</th>
+            ))}
           </tr>
         </thead>
         <tbody>
-          <tr className="bg-white">
-            <td className="px-3 py-4">1</td>
-            <td className="px-3 py-4">1</td>
-            <td className="px-3 py-4">2 x Kemeja, 1 x Ransel</td>
-            <td className="px-3 py-4">2023-10-26 23:26:00</td>
-            <td className="px-3 py-4">Rp10.000</td>
-            <td className="px-3 py-4">Dalam Proses</td>
-            <td className="px-3 py-4">Sudah Dibayar</td>
-            <td className="px-3 py-4">Lihat</td>
-            <td className="px-6 py-4">Edit</td>
-          </tr>
-          <tr className="bg-white">
-            <td className="px-3 py-4">1</td>
-            <td className="px-3 py-4">1</td>
-            <td className="px-3 py-4">2 x Kemeja, 1 x Ransel</td>
-            <td className="px-3 py-4">2023-10-26 23:26:00</td>
-            <td className="px-3 py-4">Rp10.000</td>
-            <td className="px-3 py-4">Dalam Proses</td>
-            <td className="px-3 py-4">Sudah Dibayar</td>
-            <td className="px-3 py-4">Lihat</td>
-            <td className="px-6 py-4">Edit</td>
-          </tr>
-          <tr className="bg-white">
-            <td className="px-3 py-4">1</td>
-            <td className="px-3 py-4">1</td>
-            <td className="px-3 py-4">2 x Kemeja, 1 x Ransel</td>
-            <td className="px-3 py-4">2023-10-26 23:26:00</td>
-            <td className="px-3 py-4">Rp10.000</td>
-            <td className="px-3 py-4">Dalam Proses</td>
-            <td className="px-3 py-4">Sudah Dibayar</td>
-            <td className="px-3 py-4">Lihat</td>
-            <td className="px-6 py-4">Edit</td>
-          </tr>
-          <tr className="bg-white">
-            <td className="px-3 py-4">1</td>
-            <td className="px-3 py-4">1</td>
-            <td className="px-3 py-4">2 x Kemeja, 1 x Ransel</td>
-            <td className="px-3 py-4">2023-10-26 23:26:00</td>
-            <td className="px-3 py-4">Rp10.000</td>
-            <td className="px-3 py-4">Dalam Proses</td>
-            <td className="px-3 py-4">Sudah Dibayar</td>
-            <td className="px-3 py-4">Lihat</td>
-            <td className="px-6 py-4">Edit</td>
-          </tr>
-          <tr className="bg-white">
-            <td className="px-3 py-4">1</td>
-            <td className="px-3 py-4">1</td>
-            <td className="px-3 py-4">2 x Kemeja, 1 x Ransel</td>
-            <td className="px-3 py-4">2023-10-26 23:26:00</td>
-            <td className="px-3 py-4">Rp10.000</td>
-            <td className="px-3 py-4">Dalam Proses</td>
-            <td className="px-3 py-4">Sudah Dibayar</td>
-            <td className="px-3 py-4">Lihat</td>
-            <td className="px-6 py-4">Edit</td>
-          </tr>
+          {data.map((item) => (
+            <tr key={item['ID']} className="bg-white">
+              <td className="px-3 py-4">{item["ID"]}</td>
+              <td className="px-3 py-4">{item["ID USER"]}</td>
+              <td className="px-3 py-4">{item["ITEMS"]}</td>
+              <td className="px-3 py-4">{item["WAKTU PEMESANAN"]}</td>
+              <td className="px-3 py-4">{item["NOMINAL PESANAN"]}</td>
+              <td className="px-3 py-4">{item["STATUS PESANAN"]}</td>
+              <td className="px-3 py-4">{item["STATUS PEMBAYARAN"]}</td>
+              <td className="px-3 py-4">{item["BUKTI PEMBAYARAN"]}</td>
+              <td className="px-3 py-4">{item[""]}</td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </div>
