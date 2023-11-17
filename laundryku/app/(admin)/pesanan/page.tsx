94cd2553a3ee2beddc6cc6b8874180d89119f17e
safiqq <szarifin20041@gmail.com>
2023-11-17 19:38:38 +0700

    feat: generalize table component

--- laundryku/app/(admin)/pesanan/page.tsx
+++ laundryku/app/(admin)/pesanan/page.tsx
@@ -1,19 +1,82 @@
 import Sidebar from "@/components/sidebar";
 import Searchbar from "@/components/searchbar";
-import TablePesanan from "@/components/table-pesanan";
+import Table from "@/components/table";
+
+const data = [
+  {
+    "id": 1,
+    "id_user": 1,
+    "items": '2 x Kemeja, 1 x Ransel',
+    "waktu_pemesanan": '2023-10-26 23:26:00',
+    "nominal_pesanan": 'Rp10.000',
+    "status_pesanan": 'Dalam Proses',
+    "status_pembayaran": 'Sudah Dibayar',
+    "bukti_pembayaran": 'Lihat',
+  },
+  {
+    "id": 2,
+    "id_user": 2,
+    "items": '2 x Kemeja, 1 x Ransel',
+    "waktu_pemesanan": '2023-10-26 23:26:00',
+    "nominal_pesanan": 'Rp10.000',
+    "status_pesanan": 'Dalam Proses',
+    "status_pembayaran": 'Sudah Dibayar',
+    "bukti_pembayaran": 'Lihat',
+  },
+  {
+    "id": 3,
+    "id_user": 3,
+    "items": '2 x Kemeja, 1 x Ransel',
+    "waktu_pemesanan": '2023-10-26 23:26:00',
+    "nominal_pesanan": 'Rp10.000',
+    "status_pesanan": 'Dalam Proses',
+    "status_pembayaran": 'Sudah Dibayar',
+    "bukti_pembayaran": 'Lihat',
+  },
+  {
+    "id": 4,
+    "id_user": 4,
+    "items": '2 x Kemeja, 1 x Ransel',
+    "waktu_pemesanan": '2023-10-26 23:26:00',
+    "nominal_pesanan": 'Rp10.000',
+    "status_pesanan": 'Dalam Proses',
+    "status_pembayaran": 'Sudah Dibayar',
+    "bukti_pembayaran": 'Lihat',
+  },
+  {
+    "id": 5,
+    "id_user": 5,
+    "items": '2 x Kemeja, 1 x Ransel',
+    "waktu_pemesanan": '2023-10-26 23:26:00',
+    "nominal_pesanan": 'Rp10.000',
+    "status_pesanan": 'Dalam Proses',
+    "status_pembayaran": 'Sudah Dibayar',
+    "bukti_pembayaran": 'Lihat',
+  },
+  {
+    "id": 6,
+    "id_user": 6,
+    "items": '2 x Kemeja, 1 x Ransel',
+    "waktu_pemesanan": '2023-10-26 23:26:00',
+    "nominal_pesanan": 'Rp10.000',
+    "status_pesanan": 'Dalam Proses',
+    "status_pembayaran": 'Sudah Dibayar',
+    "bukti_pembayaran": 'Lihat',
+  },
+]
 
 export default function Pesanan() {
   return (
     <main className="flex min-w-screen min-h-screen bg-[#F5F7FF]">
       <Sidebar />
       <div className="basis-1/5"></div>
-      <div className="flex flex-col basis-4/5">
+      <div className="flex flex-col basis-4/5 overflow-x-auto">
         <div className="items-center">
           <Searchbar isShow={true}></Searchbar>
         </div>
-        <div className="px-12 text-black">
+        <div className="px-12">
           <h1>Manajemen Pesanan</h1>
-          <TablePesanan/>
+          <Table data={data} rud={["u"]} />
         </div>
       </div>
     </main>
