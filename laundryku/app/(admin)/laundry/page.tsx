31e8645623a2d885993b4570ca01a17058e2a036
carissarianda <16521473@mahasiswa.itb.ac.id>
2023-11-27 19:06:58 +0700

    modal add data laundry

--- laundryku/app/(admin)/laundry/page.tsx
+++ laundryku/app/(admin)/laundry/page.tsx
@@ -5,6 +5,7 @@ import Image from "next/image";
 import Sidebar from "@/components/sidebar";
 import Searchbar from "@/components/searchbar";
 import Table from "@/components/table";
+import ModalEditData from "@/components/modal/add-item";
 
 interface Item {
   id: number;
@@ -98,38 +99,53 @@ const data: Item[] = [
   },
 ];
 
-export default function Laundry(props:{isWideScreen: boolean}) {
-  const {isWideScreen} = props;
+const Laundry = (props: { isWideScreen: boolean }) => {
+  const { isWideScreen } = props;
   const [isAddItem, setIsAddItem] = useState(false);
+  const [isEditModalOpen, setIsEditModalOpen] = useState(false); // New state for the "Edit" modal
+  const [selectedItem, setSelectedItem] = useState(null); // Track the selected item to edit
+
+  // Function to open the "Edit" modal
+  const openEditModal = (item) => {
+    setSelectedItem(item);
+    setIsEditModalOpen(true);
+  };
+
+  // Function to close the "Edit" modal
+  const closeEditModal = () => {
+    setSelectedItem(null);
+    setIsEditModalOpen(false);
+  };
 
   return (
-    // <main className="flex min-w-screen min-h-screen bg-[#F5F7FF]">
-    //   <Sidebar />
-    //   {isWideScreen && <div className="basis-1/5" />}
-    //   <div className="flex flex-col basis-4/5">
-    //     <div className="items-center">
-    //       <Searchbar isShow={false}></Searchbar>
-    //     </div>
-        <div className="px-12">
-          <div className="flex justify-between">
-            <h1>Data Laundry</h1>
-            <div
-              className="flex items-center justify-center rounded-full px-6 py-1 gap-2 bg-[#7689E7] cursor-pointer"
-              // onClick={() => ModalAddItem(onClose={() => console.log(1)})}
-              onClick={() => setIsAddItem(!isAddItem)}
-            >
-              <Image src="/logo-white/add-ring.svg" width={32} height={32} alt="Add Item"></Image>
-              <p className="text-white font-bold">ADD NEW</p>
-            </div>
-          </div>
-          <Table
-            id="table"
-            data={data}
-            allowUpdate={true}
-            allowDelete={true}
-          />
+    <div className="px-12">
+      <div className="flex justify-between">
+        <h1>Data Laundry</h1>
+        <div
+          className="flex items-center justify-center rounded-full px-6 py-1 gap-2 bg-[#7689E7] cursor-pointer"
+          onClick={() => openEditModal(!isAddItem)}
+        >
+          <Image src="/logo-white/add-ring.svg" width={32} height={32} alt="Add Item" />
+          <p className="text-white font-bold">ADD NEW</p>
         </div>
-    //   </div>
-    // </main>
+      </div>
+      <Table
+        data={data}
+        allowUpdate={true}
+        allowDelete={true}
+        onEdit={openEditModal} // Pass the function to open the "Edit" modal
+      />
+
+      {/* Edit Modal */}
+      {isEditModalOpen && (
+        <ModalEditData
+          isOpen={isEditModalOpen}
+          item={selectedItem}
+          onClose={closeEditModal}
+        />
+      )}
+    </div>
   );
-}
+};
+
+export default Laundry;
--- laundryku/components/modal/add-item.tsx
+++ laundryku/components/modal/add-item.tsx
@@ -1,10 +1,29 @@
 import React, { useState } from "react";
 import { Dropdown } from "../dropdown";
+import Image from "next/image"; // Import Image component
 
 interface ModalAddItemProps {
   isOpen: boolean;
   onClose: () => void;
 }
+
+const handleFileUpload = (files: FileList | null) => {
+  if (!files) return;
+
+  const file = files[0];
+  const reader = new FileReader();
+
+  reader.onload = (e) => {
+    // The result property contains the data as a data URL
+    const imageData = e.target?.result;
+    console.log('Image Data:', imageData);
+
+    // You can use the imageData in your application, for example, display it in an <img> tag.
+  };
+
+  // Read the file as a data URL
+  reader.readAsDataURL(file);
+};
 
 const ModalAddItem = (props: ModalAddItemProps) => {
   const { isOpen, onClose } = props;
@@ -15,15 +34,13 @@ const ModalAddItem = (props: ModalAddItemProps) => {
 
   const [kategoriProduk, setKategoriProduk] = useState("Pakaian");
   const [statusProduk, setStatusProduk] = useState("Aktif");
-  const [payload, setPayload] = useState(
-    {
-      nama: "",
-      harga: 0,
-      kategori: "",
-      status: "",
-      gambar: ""
-    }
-  );
+  const [payload, setPayload] = useState({
+    nama: "",
+    harga: 0,
+    kategori: "",
+    status: "",
+    gambar: ""
+  });
 
   const kategoriProduks = [
     {
@@ -55,87 +72,98 @@ const ModalAddItem = (props: ModalAddItemProps) => {
     },
   ];
 
-  const cssForm = "flex flex-col gap-2"
+  const cssForm = "flex flex-col gap-2";
 
   const handleSave = () => {
     console.log("Form Data:", payload);
   };
 
   return (
-    <div
-      className="fixed inset-0 z-10 flex items-center justify-center overflow-y-auto"
-    >
+    <div className="fixed inset-0 z-10 flex items-center justify-center overflow-y-auto">
       <div className="bg-white w-full max-w-lg p-6 rounded-2xl shadow-xl">
         <div className="flex flex-col mb-4">
-          <h1
-            className="text-2xl font-bold text-black flex justify-center"
-            id="modal-title"
-          >
+          <p className="text-2xl font-bold text-black flex justify-center mb-10" id="modal-title">
             Tambah Data Laundry
-          </h1>
+          </p>
           <div className="mt-4 flex flex-col gap-5">
             <div className={cssForm}>
-              <label
-                htmlFor="id_user"
-                className="text-xl font-medium text-black"
-              >
+              <label htmlFor="id_user" className="text-xl font-bold text-black">
                 Nama Produk
               </label>
               <input
                 type="text"
                 name="id_user"
                 id="id_user"
-                className="text-md py-3 px-5 border rounded-2xl border-black placeholder:text-gray-400"
+                className="text-md mb-5 py-3 px-5 border rounded-2xl border-black placeholder:text-gray-400"
                 placeholder="Nama Produk"
-                onChange={(e: React.ChangeEvent<HTMLElement>) => {
-                  setPayload({ ...payload, nama: e.target.textContent as string })
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                  setPayload({ ...payload, nama: e.target.value });
                 }}
               />
             </div>
             <div className={cssForm}>
-              <label
-                htmlFor="items"
-                className=" text-xl font-medium text-black"
-              >
+              <label htmlFor="items" className="text-xl font-bold text-black">
                 Harga Produk
               </label>
-              <input type="text" name="items" id="items" className="text-md py-3 px-5 border rounded-2xl border-black placeholder:text-gray-400" placeholder="Harga Produk"></input>
+              <input
+                type="text"
+                name="items"
+                id="items"
+                className="text-md mb-5 py-3 px-5 border rounded-2xl border-black placeholder:text-gray-400"
+                placeholder="Harga Produk"
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                  setPayload({ ...payload, harga: parseFloat(e.target.value) });
+                }}
+              />
             </div>
-            <div>
-              <div className="flex justify-between mb-2">
-                <div className={cssForm}>
-                  <label
-                    htmlFor="items"
-                    className=" text-xl font-medium text-black"
-                  >
-                    Kategori Produk
-                  </label>
-                </div>
-                <div className={cssForm}>
-                  <label
-                    htmlFor="items"
-                    className=" text-xl font-medium text-black"
-                  >
-                    Status Produk
-                  </label>
-                </div>
+            <div className="flex items-start">
+              <div className="w-full mr-2" style={{ width: "300px" }}> {/* Set the width of the dropdown here */}
+                <label
+                  htmlFor="items"
+                  className="text-xl font-bold text-black"
+                >
+                  Kategori Produk
+                </label>
+                <Dropdown  elements={kategoriProduks} type={2} />
               </div>
-              <div className="flex justify-between">
-                <Dropdown elements={kategoriProduks} type={2}></Dropdown>
-                <Dropdown elements={statusProduks} type={2}></Dropdown>
+              <div className="w-full" style={{ width: "50%" }}> {/* Set the width of the dropdown here */}
+                <label
+                  htmlFor="items"
+                  className="text-xl font-bold text-black"
+                >
+                  Status Produk
+                </label>
+                <Dropdown elements={statusProduks} type={2} />
               </div>
             </div>
           </div>
         </div>
+        {/* Additional file upload section */}
+        <div className="flex flex-col items-center mt-10">
+          <input
+            type="file"
+            id="fileInput"
+            className="hidden"
+            onChange={(e) => handleFileUpload(e.target.files)}
+          />
+          <label
+            htmlFor="fileInput"
+            className="border-dashed border-2 border-[#6C6C6C] rounded-3xl w-10/12 h-32 text-[#6C6C6C] flex flex-col justify-center items-center cursor-pointer"
+          >
+            <Image src="/upload.svg" width={50} height={50} alt="Upload" />
+            <p className="font-semibold text-base mt-2">.PDF .JPG .PNG .DOC</p>
+            <p className="font-semibold text-base">Drag and Drop your files here</p>
+          </label>
+        </div>
         <div className="flex justify-between mt-10">
           <button
-            className=" justify-center rounded-full border-[#B4ACAC] bg-white px-14 py-2 text-base font-semibold text-black ring-1 ring-inset ring-gray-300 hover:bg-[#dedcdc] sm:w-auto"
-          onClick={onClose}
+            className="justify-center rounded-full border-[#B4ACAC] bg-white px-14 py-2 text-base font-semibold text-black ring-1 ring-inset ring-gray-300 hover:bg-[#dedcdc] sm:w-auto"
+            onClick={onClose}
           >
             Cancel
           </button>
           <button
-            className="justify-center rounded-full bg-[#7689E7] px-14 py-2 text-base font-semibold text-white hover:bg-[#5365c1] sm:w-auto "
+            className="justify-center rounded-full bg-[#7689E7] px-14 py-2 text-base font-semibold text-white hover:bg-[#5365c1] sm:w-auto"
             onClick={handleSave}
           >
             Save
